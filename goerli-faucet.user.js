e13547a1e8660ac89382cba79db8f36d93fa8912
Jesse Millman <jesse@jessemillman.com.au>
2022-10-17 10:46:20 -0400

    fixed formatting

--- goerli-faucet.user.js
+++ goerli-faucet.user.js
@@ -2,7 +2,7 @@
 // @name         Goerli Faucet
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Automatically submits geth faucet. 
+// @description  Automatically submits geth faucet.
 // @author       You
 // @match        https://goerlifaucet.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=goerlifaucet.com
@@ -11,34 +11,38 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-
-    window.addEventListener("load", function(event) {
-        console.log("running")
-        var address = '0xD1bFeb2e62078eBbF4B7aB26461E0af781078631' // update with your wallet address
-        var input = document.querySelector('#root > div.alchemy-app > div.alchemy-app-content-container > div:nth-child(2) > div.row > div > span > div:nth-child(1) > div.col-md-9.col-sm-12 > input')
-        var submit = document.querySelector('#root > div.alchemy-app > div.alchemy-app-content-container > div:nth-child(2) > div.row > div > span > div:nth-child(1) > div.col-md-3.col-sm-12 > button > div > span')
-
-        setNativeValue(input, address);
-
-        setTimeout(() => {
-            submit.click()
-        }, 500)
+(function () {
+    "use strict";
+  
+    window.addEventListener("load", function (event) {
+      console.log("running");
+      var address = "0xD1bFeb2e62078eBbF4B7aB26461E0af781078631"; // update with your wallet address
+      var input = document.querySelector(
+        "#root > div.alchemy-app > div.alchemy-app-content-container > div:nth-child(2) > div.row > div > span > div:nth-child(1) > div.col-md-9.col-sm-12 > input"
+      );
+      var submit = document.querySelector(
+        "#root > div.alchemy-app > div.alchemy-app-content-container > div:nth-child(2) > div.row > div > span > div:nth-child(1) > div.col-md-3.col-sm-12 > button > div > span"
+      );
+  
+      setNativeValue(input, address);
+  
+      setTimeout(() => {
+        submit.click();
+      }, 500);
     });
-    
+  
     function setNativeValue(element, value) {
-        let lastValue = element.value;
-        element.value = value;
-        let event = new Event("input", { target: element, bubbles: true });
-        // React 15
-        event.simulated = true;
-        // React 16
-        let tracker = element._valueTracker;
-        if (tracker) {
-            tracker.setValue(lastValue);
-        }
-        element.dispatchEvent(event);
+      let lastValue = element.value;
+      element.value = value;
+      let event = new Event("input", { target: element, bubbles: true });
+      // React 15
+      event.simulated = true;
+      // React 16
+      let tracker = element._valueTracker;
+      if (tracker) {
+        tracker.setValue(lastValue);
+      }
+      element.dispatchEvent(event);
     }
-
-})();
+  })();
+  
