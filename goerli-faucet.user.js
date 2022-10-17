ac31522bd998bf79b14fb4c618bae6533fa5a9d5
Jesse Millman <jesse@jessemillman.com.au>
2022-10-17 10:39:30 -0400

    init

---
+++ goerli-faucet.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         Goerli Faucet
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Automatically submits geth faucet. 
+// @author       You
+// @match        https://goerlifaucet.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=goerlifaucet.com
+// @updateURL    https://github.com/jessemillman/geth-tampermonkey/raw/main/goerli-faucet.user.js
+// @downloadURL  https://github.com/jessemillman/geth-tampermonkey/raw/main/goerli-faucet.user.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.addEventListener("load", function(event) {
+        console.log("running")
+        var address = '0xD1bFeb2e62078eBbF4B7aB26461E0af781078631' // update with your wallet address
+        var input = document.querySelector('#root > div.alchemy-app > div.alchemy-app-content-container > div:nth-child(2) > div.row > div > span > div:nth-child(1) > div.col-md-9.col-sm-12 > input')
+        var submit = document.querySelector('#root > div.alchemy-app > div.alchemy-app-content-container > div:nth-child(2) > div.row > div > span > div:nth-child(1) > div.col-md-3.col-sm-12 > button > div > span')
+
+        setNativeValue(input, address);
+
+        setTimeout(() => {
+            submit.click()
+        }, 2000)
+    });
+    
+    function setNativeValue(element, value) {
+        let lastValue = element.value;
+        element.value = value;
+        let event = new Event("input", { target: element, bubbles: true });
+        // React 15
+        event.simulated = true;
+        // React 16
+        let tracker = element._valueTracker;
+        if (tracker) {
+            tracker.setValue(lastValue);
+        }
+        element.dispatchEvent(event);
+    }
+
+})();
